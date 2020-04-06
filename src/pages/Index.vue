2e40e2b2a107eb92f9c16c2c75723c0eb5d0f1c3
Gustavo <gustavo@wyasi.com.br>
2020-04-06 11:02:28 -0300

    video topo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -17,7 +17,7 @@
                   <iframe
                     width="956"
                     height="538"
-                    src="https://www.youtube.com/embed/GhAqAiYYdCc"
+                    src="https://www.youtube.com/embed/5MApz1nUIiQ"
                     frameborder="0"
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen
