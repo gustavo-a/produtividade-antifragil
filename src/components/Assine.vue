14696cb22bf193b1819374a20f2510935e4c6dd0
Gustavo <gustavo@wyasi.com.br>
2020-04-09 17:19:48 -0300

    clique assine + texto cliente

--- src/components/Assine.vue
+++ src/components/Assine.vue
@@ -2,17 +2,22 @@
   <section class="bg-cafe-meia-noite py-32" id="assine-bloco">
     <div class="container px-4">
       <div class="flex justify-center mb-16">
-        <h2 class="title-1">Assine agora</h2>
+        <h2 class="title-1">
+          <a href="https://hotm.art/oOizV2">Assine agora</a>
+        </h2>
       </div>
       <div class="flex flex-wrap md:justify-around">
         <div
           class="w-full md:w-1/2 lg:w-1/3 rounded-lg bg-gray-100 text-center px-4 py-10 self-center mb-16 md:mb-0"
         >
           <Time class="m-auto mb-4" />
-          <span>Você investe apenas:</span>
           <div
-            class="grid font-sans-secondary text-cafe-verde-amarelado my-4 italic"
+            class="font-sans-secondary text-cafe-verde-amarelado text-3xl mb-2"
           >
+            De R$ <span class="line-through">469,00</span>
+          </div>
+          <span>Por apenas:</span>
+          <div class="grid font-sans-secondary text-cafe-verde-amarelado my-4">
             <div class="text-2xl">10x de</div>
             <div class="text-5xl">R$ 22,90</div>
           </div>
@@ -20,24 +25,27 @@
         </div>
         <div class="w-full md:w-1/3 text-white">
           <p class="mb-4 font-bold">
-            Um curso sobre Produtividade como você nunca viu igual, garanto.
-            Completo, imersivo, profundo e com material de apoio que em sua
-            maior parte você não encontra aqui no Brasil.
+            Um curso sobre Produtividade para quem quer ir além das ações
+            táticas e mergulhar na estratégia: desenvolver um Olhar Produtivo.
+            Provocativo, irreverente e com um material de apoio riquíssimo que
+            em sua maior parte você não encontra aqui no Brasil.
           </p>
           <ul class="lista">
-            <li>80% de desconto para a primeira turma</li>
+            <li>50% de desconto para a primeira turma</li>
             <li>
-              Acesso ilimitado por 24 meses (vitalício para os assinantes do CB
-              Premium)
+              Acesso ilimitado por 12 meses (vitalício para os assinantes do
+              Café Brasil Premium)
             </li>
             <li>
-              Grupo exclusivo no telegram para perguntas, respostas e troca de
+              Grupo exclusivo no Telegram para perguntas, respostas e troca de
               ideias em relação ao tema
             </li>
-            <li>Aulas ao vivo com o Luciano Pires</li>
+            <li>Live semanal com o autor Luciano Pires</li>
             <li>
-              Forma interativa que permite aplicação de imediato no dia a dia
+              Forma interativa que permite consumo em vídeo, áudio ou texto,
+              quando, onde, como e quanto você quiser
             </li>
+            <li>Conteúdo com aplicação imediata no dia a dia</li>
             <li>
               Oportunidade de adquirir novos hábitos e habilidades que vão
               transformar sua visão de mundo e maneira de fazer e consumir.
