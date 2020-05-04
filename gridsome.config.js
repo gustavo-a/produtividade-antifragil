147d3485b3e0c8a76937c739837681c95458e47e
Gustavo <gustavo@wyasi.com.br>
2020-05-04 17:50:18 -0300

    ms edge

--- gridsome.config.js
+++ gridsome.config.js
@@ -23,7 +23,12 @@ module.exports = {
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
-    svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
   },
   plugins: [
     {
