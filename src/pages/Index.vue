b7221ac71b727df634015e5c0a24fa9498dec4bb
Gustavo <gustavo_641@live.com>
2020-06-18 17:43:09 -0300

    bug popup blank space

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -295,22 +295,24 @@ ul.lista {
     @apply .text-xl;
   }
 }
+.v--modal-box,
+.popup__content {
+  .video-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px;
+    height: 0;
+    overflow: hidden;
+  }
 
-.video-container {
-  position: relative;
-  padding-bottom: 56.25%;
-  padding-top: 30px;
-  height: 0;
-  overflow: hidden;
-}
-
-.video-container iframe,
-.video-container object,
-.video-container embed {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  .video-container iframe,
+  .video-container object,
+  .video-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
